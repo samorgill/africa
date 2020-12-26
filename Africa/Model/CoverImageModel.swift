19289ab3ff24c19e167ac3b8662c7b8af909c28e
Sam Orgill <ssorgill@icloud.com>
2020-12-26 15:42:36 +0000

    Cover images

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  cover.swift
+//  Africa
+//
+//  Created by Sam Orgill on 26/12/2020.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
