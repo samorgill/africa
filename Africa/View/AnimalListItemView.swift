a149f6884cc1d2cc1db409b605f9818400469d4a
Sam Orgill <ssorgill@icloud.com>
2020-12-27 15:44:11 +0000

    List of animals

---
+++ Africa/View/AnimalListItemView.swift
@@ -0,0 +1,49 @@
+//
+//  AnimalListItemView.swift
+//  Africa
+//
+//  Created by Sam Orgill on 27/12/2020.
+//
+
+import SwiftUI
+
+struct AnimalListItemView: View {
+    // MARK: - PROPERTIES
+    let animal: Animal
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 16) {
+            Image(animal.image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 90, height: 90)
+                .clipShape(
+                    RoundedRectangle(cornerRadius: 12)
+                )
+            VStack(alignment: .leading, spacing: 8) {
+                Text(animal.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                
+                Text(animal.description)
+                    .font(.footnote)
+                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                    .lineLimit(2)
+                    .padding(.trailing, 8)
+            }
+        } //: HSTACK
+    }
+}
+
+// MARK: - PREVIEW
+struct AnimalListItemView_Previews: PreviewProvider {
+    
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    static var previews: some View {
+        AnimalListItemView(animal: animals[1])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
