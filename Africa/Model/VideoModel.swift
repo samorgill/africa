93cd470ad1d7b78e2344ea97249fa7144c78ab6a
Sam Orgill <ssorgill@icloud.com>
2021-01-04 19:54:53 +0000

    Video list item

---
+++ Africa/Model/VideoModel.swift
@@ -0,0 +1,16 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by Sam Orgill on 04/01/2021.
+//
+
+import SwiftUI
+
+struct Video: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+}
+
+
---
+++ Africa/View/VideoListItem.swift
@@ -0,0 +1,56 @@
+//
+//  VideoListItem.swift
+//  Africa
+//
+//  Created by Sam Orgill on 04/01/2021.
+//
+
+import SwiftUI
+
+struct VideoListItem: View {
+    // MARK: - PROPERTIES
+    
+    let video: Video    
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack(spacing: 10) {
+            ZStack {
+                Image("video-\(video.id)")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 80)
+                    .cornerRadius(9)
+                Image(systemName: "play.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 32)
+                    .shadow(radius: 4)
+            } //: ZSTACK
+            
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text(video.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                Text(video.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                    .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                
+            }
+        } //: HSTACK
+    }
+}
+
+// MARK: - PREVIEWS
+struct VideoListItem_Previews: PreviewProvider {
+    static let videos: [Video] = Bundle.main.decode("videos.json")
+    
+    static var previews: some View {
+        VideoListItem(video: videos[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
