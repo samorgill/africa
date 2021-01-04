fe3db6726c14a2e22203cfff3389fa23b13f6fb3
Sam Orgill <ssorgill@icloud.com>
2021-01-04 21:12:18 +0000

    Haptics to button

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct VideoListView: View {
+    //: - PROPERTIES
+    
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+
+    //: - BODY
     var body: some View {
-        Text("Videos")
+        
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                } //: LOOP
+            } //: LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle("Videos")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        // Shuffle videos
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                    
+                }
+            }
+            
+        } //: NAVIGATION
     }
 }
 
+// :- PREVIEW
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
