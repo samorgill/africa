8b3e8b175419fe3e0ccdfd78f430f8548b065ce5
Sam Orgill <ssorgill@icloud.com>
2021-01-10 17:13:40 +0000

    Dynamic grid lists and haptics

--- Africa/App/GallaryView.swift
+++ Africa/App/GallaryView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 struct GallaryView: View {
     // MARK: - PROPERTIES
     
+    @State private var selectedAnimal: String = "lion"
+    
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    let hapticSoft = UIImpactFeedbackGenerator(style: .soft)
+    let hapticLight = UIImpactFeedbackGenerator(style: .light)
     
     // SIMPLE GRID DEFINITION
     //    let gridLayout: [GridItem] = [
@@ -20,11 +24,18 @@ struct GallaryView: View {
     //    ]
     
     // EFFICIENT GRID DEFINITION
+//    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
     
-    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    // DYNAMIC GRID LAYOUT
     
-    @State private var selectedAnimal: String = "lion"
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
     
+    //
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+        
     // MARK: - BODY
     
     var body: some View {
@@ -36,6 +47,15 @@ struct GallaryView: View {
                 .scaledToFit()
                 .clipShape(Circle())
                 .overlay(Circle().stroke(Color.white, lineWidth: 8))
+            
+            // MARK: - SLIDER
+            Slider(value: $gridColumn, in: 2...4, step: 1)
+                .padding(.horizontal)
+                .onChange(of: gridColumn, perform: { value in
+                    gridSwitch()
+                    hapticLight.impactOccurred()
+                })
+            
             VStack(alignment: .center, spacing: 30) {
                 // MARK: - GRID
                 
@@ -48,9 +68,14 @@ struct GallaryView: View {
                             .overlay(Circle().stroke(Color.white, lineWidth: 1))
                             .onTapGesture {
                                 selectedAnimal = item.image
+                                hapticSoft.impactOccurred()
                             }
                     } //: LOOP
                 }) //: GRID
+                .animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
+                .onAppear(perform: {
+                    gridSwitch()
+                })
             } //: VSTACK
             .padding(.horizontal, 10)
             .padding(.vertical, 15)
