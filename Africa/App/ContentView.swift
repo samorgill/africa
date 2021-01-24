38e240913d91d3579a1cb77b3ee5944e418c9e6c
Sam Orgill <ssorgill@icloud.com>
2021-01-24 16:10:28 +0000

    Grids of animals

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -14,22 +14,41 @@ struct ContentView: View {
     let haptics = UIImpactFeedbackGenerator(style: .medium)
     @State private var isGridViewActive: Bool = false
     
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+    
     // MARK: - BODY
     var body: some View {
         
+        
+        
         NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                ForEach(animals) { animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)
-                    ) {
-                        AnimalListItemView(animal: animal)
-                    }
-                }
-                .padding(.vertical)
-            } //: LIST
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)
+                            ) {
+                                AnimalListItemView(animal: animal)
+                            }
+                        }
+                        .padding(.vertical)
+                    } //: LIST
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10, content: {
+                            ForEach(animals) { animal in
+                                NavigationLink(
+                                    destination: AnimalDetailView(animal: animal)) {
+                                    AnimalGridItemView(animal: animal)
+                                } //: LINK
+                            } //: LOOP
+                        }) //: GRID
+                    } //: SCROLL
+                } //: CONDITION
+            } //: GROUP
             .navigationBarTitle("Africa", displayMode: .large)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -44,15 +63,15 @@ struct ContentView: View {
                                 .font(.title2)
                                 .foregroundColor(isGridViewActive ? .primary : .accentColor)
                         }
-                    
+                        
                         
                         // GRID
                         Button(action: {
                             print("Grid view activated")
                             isGridViewActive = true
-
+                            
                             haptics.impactOccurred()
-
+                            
                         }) {
                             Image(systemName: "square.grid.2x2")
                                 .font(.title2)
